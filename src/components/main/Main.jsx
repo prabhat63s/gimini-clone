84113f274d4c3a591f2b50b91846af5f3e5df865
prabhat63s <prabhat6914@gmail.com>
2024-03-05 13:40:43 +0530

    first commit

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,85 @@
+import { FaPen, FaRegCompass } from "react-icons/fa6";
+import "./Main.css";
+import { SlPlane } from "react-icons/sl";
+import { LuImagePlus, LuLightbulb, LuMic, LuSend } from "react-icons/lu";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
+import gemini from "../../assets/logo.webp";
+import user from "../../assets/user.jpeg";
+
+const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
+  return (
+    <div className="main">
+      <div className="nav">
+        <p className="">Main baba tillu</p>
+        <img src={user} alt="" />
+      </div>
+
+      <div className="main-container">
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Main baba tillu!</span>
+              </p>
+              <p className="bio">B.Tech from Kanpur.</p>
+              <p>How can I help you today?</p>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={user} alt="" />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={gemini} alt="" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
+          </div>
+        )}
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Apana question pucho..."
+            />
+            <div className="">
+              <LuImagePlus className="input-icon" />
+              <LuMic className="input-icon" />
+              {input ? (
+                <LuSend className="input-icon" onClick={() => onSent()} />
+              ) : null}
+            </div>
+          </div>
+          <p className="bottom-info">
+            Hello, Main baba tillu! B.Tech from Kanpur.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,68 @@
+import { useContext, useState } from "react";
+import "./Sidebar.css";
+// import logo from "../../assets/logo.webp";
+import {
+  MdOutlineAdd,
+  MdOutlineHistory,
+  MdOutlineMenu,
+  MdOutlineMessage,
+  MdOutlineSettings,
+  MdQuestionMark,
+} from "react-icons/md";
+import { Context } from "../../context/Context";
+
+const Sidebar = () => {
+  const [extended, seExtended] = useState(false);
+  const { onSent, prevPrompt, setRecentPrompt, newChat } = useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <MdOutlineMenu
+          className="menu"
+          size={20}
+          onClick={() => seExtended((prev) => !prev)}
+        />
+        <div className="new-chat" onClick={() => newChat()}>
+          <MdOutlineAdd size={20} />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            {prevPrompt.map((item, i) => {
+              return (
+                <div className="recent-entry" key={i} onClick={() => loadPrompt(item)}>
+                  <MdOutlineMessage size={20} />
+                  <p>{item.slice(0, 18)}...</p>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <MdQuestionMark size={20} />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <MdOutlineHistory size={20} />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <MdOutlineSettings size={20} />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
